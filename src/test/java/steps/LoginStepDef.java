b5d50e23d3288d0d08affe23394436f7636759c7
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2020-10-16 23:19:52 +0300

    step was updated: * LoginStepDef class

--- src/test/java/steps/LoginStepDef.java
+++ src/test/java/steps/LoginStepDef.java
@@ -1,5 +1,6 @@
 package steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import pages.CreateAccountPage;
 import pages.MainPage;
@@ -11,13 +12,28 @@ public class LoginStepDef {
         new MainPage().clickCreateAccount();
     }
 
-    @Then("fill registration fields")
-    public void fillRegistrationFields() {
-        new CreateAccountPage().fillRegistrationFields();
-    }
-
     @Then("click start learning")
     public void clickStartLearning() {
         new CreateAccountPage().clickStartLearning();
+    }
+
+    @And("fill email field as {string}")
+    public void fillEmailFieldAs(String email) {
+        new CreateAccountPage().fillEmailField(email);
+    }
+
+    @And("fill name field as {string}")
+    public void fillNameFieldAs(String name) {
+        new CreateAccountPage().fillNameField(name);
+    }
+
+    @And("fill last name field as {string}")
+    public void fillLastNameFieldAs(String lastName) {
+        new CreateAccountPage().fillLastNameField(lastName);
+    }
+
+    @And("fill password field as {string}")
+    public void fillPasswordFieldAs(String password) {
+        new CreateAccountPage().fillPasswordField(password);
     }
 }
