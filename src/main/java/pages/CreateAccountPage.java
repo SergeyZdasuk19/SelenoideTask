95052db6b527042fcdeea6448b90b61629f3896e
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2020-10-30 20:24:49 +0300

    pages were updated: * CreateAccountPage class * MainPage class

--- src/main/java/pages/CreateAccountPage.java
+++ src/main/java/pages/CreateAccountPage.java
@@ -1,26 +1,17 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
 import pagesElements.Button;
-import pagesElements.Header;
 import pagesElements.TextField;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CreateAccountPage {
 
-    private Header header = new Header($x("//h1[text()='Create an Account to Get Started']"));
     private TextField emailField = new TextField($x("//input[@id='email']"));
     private TextField nameField = new TextField($x("//input[@id='firstName']"));
     private TextField lastNameField = new TextField($x("//input[@id='lastName']"));
     private TextField passwordField = new TextField($x("//input[@id='password']"));
     private Button startLearningBnt = new Button($x("//button[@type='submit']"));
-
-    public CreateAccountPage() {
-        header
-                .getElement()
-                .shouldBe(Condition.appear);
-    }
 
     public CreateAccountPage fillEmailField(String email) {
         emailField.addTextInField(email);
--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
 import lombok.Data;
 import pagesElements.Button;
 
@@ -10,12 +9,6 @@ import static com.codeborne.selenide.Selenide.$x;
 public class MainPage {
 
     private Button createAccountBtn = new Button($x("//span[text()='Create Account']/parent::button"));
-
-    public MainPage() {
-        createAccountBtn
-                .getElement()
-                .shouldBe(Condition.appear);
-    }
 
     public MainPage clickCreateAccount() {
         createAccountBtn.pressButton();
